45f98aae971434a4e3d7ceafbb5f147a3a6fc2b3
Administrador <administrador@macbook-pro-de-administrador.local>
2016-05-14 17:31:54 -0500

    Tarea Json

--- PeticionAlServidor/ViewController.swift
+++ PeticionAlServidor/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var isbn: UITextField!
     @IBOutlet weak var respuestaServidor: UITextView!
     
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var autor: UILabel!
+    @IBOutlet weak var portada: UILabel!
+    
+    
     @IBAction func limpiarISBN(sender: AnyObject) {
         self.isbn.text = ""
     }
@@ -39,9 +44,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
             
             let url = NSURL(string: urls)
             let datos:NSData? = NSData(contentsOfURL: url!)
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            print(texto!)
-            self.respuestaServidor.text = texto! as String
+            //let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            
+            
+            do{
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                print(json)
+                
+                if json.count > 0{
+                    let response = json as! NSDictionary
+                    let dico1 = response["ISBN:"+self.isbn.text!] as! NSDictionary
+                    let dico2 = dico1["authors"] as! NSArray
+                    let dico3 =  dico1["title"] as! NSString as String
+                    
+                    print(dico3)
+                    self.titulo.text = dico3
+                    
+                    for dico in dico2 {
+                        print(dico["name"] as! NSString as String)
+                        
+                        self.autor.text =  self.autor.text! + (dico["name"] as! NSString as String) 
+                    }
+                    
+                } else {
+                    let alert = UIAlertController(title: "Error en búsqueda", message: "ISBN no encontrado", preferredStyle: .Alert)
+                    alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: {
+                        (action) -> Void in
+                        print("No se encontró el ISBN")
+                        self.titulo.text = ""
+                        self.autor.text = ""
+                    })))
+                    
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                
+            }
+            catch _ {
+                print("Error")
+            }
+            
+            //print(texto!)
+            //self.respuestaServidor.text = texto! as String
         } else {                            //en caso que falle la conexión
             print("Internet connection FAILED")
             let alert = UIAlertController(title: "Falla en conexión", message: "Asegurate de tener conexión a internet.", preferredStyle: .Alert)
@@ -57,6 +100,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func textFieldDoneEditing(sender: UIScrollView){   //al darle enter desaparece el teclado
         sender.resignFirstResponder() //desaparece el teclado
+        
+        self.titulo.text = ""
+        self.autor.text = ""
         sincrono()
     }
     
